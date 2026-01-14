6c4809155143da0b68d3c0298e3ae9619f402cdf
Tomáš Mrázek <tomasmrazek92@gmail.com>
2026-01-14 23:03:05 +0100

    Careers - Update
    Moved from Greenhouse to Gem.com

--- src/careers.js
+++ src/careers.js
@@ -1,172 +1,173 @@
-// Career Listing
-// if location host is modular-dev.webflow.io, add class .webflow-page-type-career-post to body
-if (window.location.host === 'modular-dev.webflow.io') {
-  var greenhouse = 'modtestingsite';
+if (window.location.host === 'modular-rebuild.webflow.io') {
+  var url = 'https://api.gem.com/job_board/v0/modular/job_posts/';
 } else {
-  greenhouse = 'modularai';
+  url = 'https://boards-api.greenhouse.io/v1/boards/modularai/jobs';
 }
 
 var hostName = window.location.hostname;
 
-// get jobs for department with id 4001072005
-function getJobsForDepartment(jobPositions, departmentId) {
-  let jobsForDepartment = [];
-  for (let i = 0; i < jobPositions.departments.length; i++) {
-    if (jobPositions.departments[i].id === departmentId) {
-      jobsForDepartment = jobPositions.departments[i].jobs;
-    }
-  }
-  return jobsForDepartment;
+function getJobsForDepartment(jobs, departmentId) {
+  return jobs.filter((job) => {
+    return job.departments && job.departments.length > 0 && job.departments[0].id === departmentId;
+  });
 }
 
-// for each job in jobsForDepartment append a job to the element with class .greenhouse-tabs-content
-function appendJobsForDepartment(jobsForDepartment) {
-  document.querySelector('.roles-list_item').innerHTML = '';
-  for (let i = 0; i < jobsForDepartment.length; i++) {
-    let job = jobsForDepartment[i];
-    let jobTitle = job.title;
-    let jobId = job.id;
-    let jobLocation = job.location.name;
+function appendJobs(jobs) {
+  const container = $('.roles-list_item');
+  container.html('');
 
-    let listItem = document.createElement('li');
+  jobs.forEach((job) => {
+    const jobTitle = job.title;
+    const jobId = job.id;
+    const jobLocation = job.location.name || 'Remote';
 
-    let jobListItem = document.createElement('a');
-    jobListItem.classList.add('roles-list_link');
-    jobListItem.setAttribute(
-      'href',
-      `https://${hostName}/company/career-post?${jobId}&gh_jid=${jobId}`
-    );
-    jobListItem.setAttribute('data-job-id', jobId);
-    jobListItem.innerHTML = `<div class="z-index-2"><div class="margin-bottom margin-4"><p class="text-size-small text-weight-medium text-style-tthoves">${jobTitle}</p></div><div class="text-color-twilight60"><p class="text-size-xsmall">${jobLocation}</p></div></div> <div class="text-color-twilight60 z-index-2"><div><p class="text-size-xsmall">Apply now</p></div></div><div class="roles-list_link-bg"></div>`;
+    const listItem = $('<li>');
+    const jobLink = $('<a>')
+      .addClass('roles-list_link')
+      .attr('href', `https://${hostName}/company/career-post?${jobId}&gh_jid=${jobId}`)
+      .attr('data-job-id', jobId).html(`
+        <div class="z-index-2">
+          <div class="margin-bottom margin-4">
+            <p class="text-size-small text-weight-medium text-style-tthoves">${jobTitle}</p>
+          </div>
+          <div class="text-color-twilight60">
+            <p class="text-size-small">${jobLocation}</p>
+          </div>
+        </div>
+        <div class="text-color-twilight60 z-index-2">
+          <div><p class="text-size-small">Apply now</p></div>
+        </div>
+        <div class="roles-list_link-bg"></div>
+      `);
 
-    listItem.appendChild(jobListItem);
-    document.querySelector('.roles-list_item').appendChild(listItem);
-  }
+    listItem.append(jobLink);
+    container.append(listItem);
+  });
 }
 
-if (document.querySelector('.roles_wrap')) {
+if ($('.roles_wrap').length) {
   async function fetchData() {
-    const jobsResponse = await fetch(
-      `https://boards-api.greenhouse.io/v1/boards/${greenhouse}/jobs`
+    const response = await fetch(url);
+    const jobs = await response.json();
+    console.log(jobs);
+
+    const tabsMenu = $('.roles_wrap .roles-filters');
+    tabsMenu.html('');
+
+    const allTab = $('<li>').append(
+      $('<a>')
+        .addClass('tabs-item')
+        .attr('href', '#')
+        .html(`<div>All</div><div>(${jobs.length})</div>`)
+        .on('click', function (e) {
+          e.preventDefault();
+          $('.tabs-item').removeClass('is-active');
+          $(this).addClass('is-active');
+          appendJobs(jobs);
+        })
     );
-    const jobsData = await jobsResponse.json();
+    tabsMenu.append(allTab);
 
-    function appendAllJobs(jobs) {
-      const tabsMenu = document.querySelector('.roles_wrap .roles-filters');
-      tabsMenu.innerHTML = '';
+    const departmentMap = new Map();
 
-      const listItem = document.createElement('li');
-
-      const departmentListItem = document.createElement('a');
-      departmentListItem.classList.add('tabs-item');
-      departmentListItem.href = '#';
-      departmentListItem.innerHTML = `
-                <div>All</div>
-                <div>(${jobs.length})</div>`;
-
-      $(departmentListItem).on('click', function (e) {
-        $('.is-active').removeClass('is-active');
-        $(this).addClass('is-active');
-
-        appendJobsForDepartment(jobs);
-      });
-
-      listItem.appendChild(departmentListItem);
-      tabsMenu.appendChild(listItem);
-    }
-    appendAllJobs(jobsData.jobs);
-
-    const departmentsResponse = await fetch(
-      `https://boards-api.greenhouse.io/v1/boards/${greenhouse}/departments`
-    );
-    const departmentsData = await departmentsResponse.json();
-
-    var jobPositions = departmentsData;
-
-    function findDepartmentsWithJobs(jobPositions) {
-      let departmentsWithJobs = [];
-
-      for (let i = 0; i < jobPositions.departments.length; i++) {
-        if (jobPositions.departments[i].jobs.length > 0) {
-          departmentsWithJobs.push(jobPositions.departments[i]);
+    jobs.forEach((job) => {
+      if (job.departments && job.departments.length > 0) {
+        const dept = job.departments[0];
+        if (!departmentMap.has(dept.id)) {
+          departmentMap.set(dept.id, {
+            id: dept.id,
+            name: dept.name,
+            jobs: [],
+          });
         }
+        departmentMap.get(dept.id).jobs.push(job);
       }
+    });
 
-      return departmentsWithJobs;
-    }
+    departmentMap.forEach((department) => {
+      const deptTab = $('<li>').append(
+        $('<a>')
+          .addClass('tabs-item')
+          .attr('href', '#')
+          .attr('data-department-id', department.id)
+          .html(`<div>${department.name}</div><div>(${department.jobs.length})</div>`)
+          .on('click', function (e) {
+            e.preventDefault();
+            $('.tabs-item').removeClass('is-active');
+            $(this).addClass('is-active');
+            appendJobs(department.jobs);
+          })
+      );
+      tabsMenu.append(deptTab);
+    });
 
-    var departmentsWithJobs = findDepartmentsWithJobs(jobPositions);
-
-    function appendDepartmentsWithJobs(departmentsWithJobs) {
-      const tabsMenu = document.querySelector('.roles_wrap .roles-filters');
-
-      departmentsWithJobs.forEach((department) => {
-        const { id: departmentId, name: departmentName, jobs } = department;
-
-        const listItem = document.createElement('li');
-
-        const departmentListItem = document.createElement('a');
-
-        departmentListItem.classList.add('tabs-item');
-        departmentListItem.dataset.departmentId = departmentId;
-        departmentListItem.href = '#';
-        departmentListItem.innerHTML = `
-                        <div>${departmentName}</div>
-                        <div>(${jobs.length})</div>
-                    `;
-
-        departmentListItem.addEventListener('click', (e) => {
-          const currentTab = document.querySelector('.is-active');
-          if (currentTab) currentTab.classList.remove('is-active');
-
-          const targetLink = e.currentTarget.closest('a') || e.currentTarget.querySelector('a');
-          if (targetLink) targetLink.classList.add('is-active');
-
-          const jobsForDepartment = getJobsForDepartment(jobPositions, departmentId);
-          appendJobsForDepartment(jobsForDepartment);
-        });
-
-        listItem.appendChild(departmentListItem);
-        tabsMenu.appendChild(listItem);
-      });
-    }
-
-    appendDepartmentsWithJobs(departmentsWithJobs);
-
-    document.querySelector('.roles-filters').querySelectorAll('.tabs-item')[0].click();
+    $('.tabs-item').first().trigger('click');
   }
 
   fetchData();
 }
 
-// Career Detail
 if (window.location.pathname === '/company/career-post') {
-  if (window.location.host === 'modular-dev.webflow.io') {
-    var greenhouse = 'modtestingsite';
-  } else {
-    greenhouse = 'modularai';
-  }
+  const jobId = window.location.search.split('=')[1];
 
-  var greenhouseSrc = `https://boards.greenhouse.io/embed/job_board/js?for=${greenhouse}`;
-  var greenhouseScript = document.createElement('script');
-  greenhouseScript.src = greenhouseSrc;
-  document.body.appendChild(greenhouseScript);
-
-  var jobId = window.location.search.split('=')[1];
-  fetch(`https://boards-api.greenhouse.io/v1/boards/${greenhouse}/jobs/${jobId}`)
+  fetch(url)
     .then((response) => response.json())
-    .then((data) => {
-      if (data.status === 404) {
+    .then((jobs) => {
+      const job = jobs.find((j) => j.id.toString() === jobId);
+
+      if (!job) {
         window.location.href = '/company/careers';
         return;
       }
-      let job = data;
-      let jobTitle = job.title;
-      let jobLocation = job.location.name;
-      document.getElementById('job-title').innerHTML = jobTitle;
-      document.getElementById('job-location').innerHTML = jobLocation;
-      document.getElementById('job-breadcrumb').innerHTML = jobTitle;
-    });
 
-  document.getElementById('job-image').src = images[randomImage - 1];
+      const jobTitle = job.title;
+      const jobLocation = job.location.name || 'Remote';
+      const applyUrl = job.absolute_url;
+
+      $('#job-title').html(jobTitle);
+      $('#job-location').html(jobLocation);
+      $('#job-breadcrumb').html(jobTitle);
+
+      const iframe = $('<iframe>')
+        .attr({
+          src: applyUrl,
+          width: '100%',
+          frameborder: '0',
+          scrolling: 'no',
+        })
+        .css({
+          minHeight: '100vh',
+          display: 'block',
+        });
+
+      $('#grnhse_app').html(iframe);
+
+      window.addEventListener('message', function (e) {
+        if (e.data.height) {
+          iframe.css('height', e.data.height + 50 + 'px');
+        }
+      });
+
+      iframe.on('load', function () {
+        const checkHeight = setInterval(function () {
+          try {
+            const iframeDoc = iframe[0].contentDocument || iframe[0].contentWindow.document;
+            const height = $(iframeDoc).find('body').outerHeight();
+            if (height > 0) {
+              iframe.css('height', height + 50 + 'px');
+            }
+          } catch (e) {
+            clearInterval(checkHeight);
+          }
+        }, 500);
+
+        setTimeout(() => clearInterval(checkHeight), 5000);
+      });
+
+      $('.main-wrapper').css('opacity', '1');
+    })
+    .catch((error) => {
+      console.error('Error fetching job:', error);
+      window.location.href = '/company/careers';
+    });
 }
